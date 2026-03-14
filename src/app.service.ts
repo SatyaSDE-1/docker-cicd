1b6c59dc34e23938bfc9e622a61f5c55e8b2f774
SatyaSDE-1 <satya@flipitnews.com>
2026-03-15 00:43:26 +0530

    satya

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World hello!';
+    return 'Hello World hello! i am fine';
   }
 }
