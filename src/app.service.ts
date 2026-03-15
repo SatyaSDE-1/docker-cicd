debc34970818ac45c6e108718c429efd6323c8fe
SatyaSDE-1 <satya@flipitnews.com>
2026-03-15 10:59:58 +0530

    satya

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World hello! i am fine i am also fine ';
+    return 'Hello World hello! i am fine i am also fine my name is satya singh from gurugram  ';
   }
 }
